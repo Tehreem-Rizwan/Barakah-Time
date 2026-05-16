ea0780ae545d5be6ac0f4d77d644a92fe1f020ca
DevEntia <deventia@apps-MacBook-Air.local>
2026-05-16 18:12:40 +0500

    code updated

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,13 +9,14 @@ plugins {
 }
 
 android {
-    namespace = "com.example.barakah_time"
+    namespace = "com.tehreem.barakah_time"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -24,13 +25,14 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.barakah_time"
+        applicationId = "com.tehreem.barakah_time"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
     }
 
     buildTypes {
@@ -44,4 +46,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
--- android/app/src/main/kotlin/com/example/barakah_time/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.barakah_time
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
